703511824585f0fca4a4b075be1027aac4fe1377
lonelyprogrammer007 <108358225+lonelyprogrammer007@users.noreply.github.com>
2023-04-19 09:20:52 -0500

    1 file modified, 1 added

--- src/app/countdown-timer.component.ts
+++ src/app/countdown-timer.component.ts
@@ -2,21 +2,20 @@ import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-countdown-timer',
-  template: '<p>{{message}}</p>'
+  template: '<p>{{message}}</p>',
 })
 export class CountdownTimerComponent implements OnDestroy {
+  private clearTimer: VoidFunction | undefined;
   message = '';
   seconds = 11;
 
-  ngOnDestroy() { this.clearTimer?.(); }
-
-  start() { this.countDown(); }
-  stop()  {
+  start() {
+    this.countDown();
+  }
+  stop() {
     this.clearTimer?.();
     this.message = `Holding at T-${this.seconds} seconds`;
   }
-
-  private clearTimer: VoidFunction | undefined;
 
   private countDown() {
     this.clearTimer?.();
@@ -25,14 +24,19 @@ export class CountdownTimerComponent implements OnDestroy {
       if (this.seconds === 0) {
         this.message = 'Blast off!';
       } else {
-        if (this.seconds < 0) { this.seconds = 10; } // reset
+        if (this.seconds < 0) {
+          this.seconds = 10;
+        } // reset
         this.message = `T-${this.seconds} seconds and counting`;
       }
     }, 1000);
     this.clearTimer = () => clearInterval(interval);
   }
-}
 
+  ngOnDestroy() {
+    this.clearTimer?.();
+  }
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
